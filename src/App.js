553f05b28be15666a5a444745a6ce7933c369f59
PravinPatil9 <patilpravin904@gmail.com>
2023-06-13 22:54:09 +0530

    demo added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import MyComponent from './components/MyComponent';
 import RegistrationForm from './components/RegistrationForm';
+import Demo from './components/Demo';
 
 function App() {
   return (
     <div>
       <RegistrationForm/>
      <MyComponent/>
+     <Demo/>
     </div>
   );
 }
